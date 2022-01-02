baee47bb470c445ccda329398fcb850e31d7ee94
Ana Fernández Valdés <91973567+heyanabanana@users.noreply.github.com>
2022-01-02 22:12:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  var owl = $('.owl-carousel');
+
+owl.owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  // autoplay:true,
+  // autoplayTimeout:5000,
+  items: 1,
+  animateOut: 'fadeOut'
+
+});
+$(".owl-next").html(
+  '<img class="arrow-icon arrow-icon-next" src="assets/icons/nav-icon-right.svg"/>'
+);
+$(".owl-prev").html(
+  '<img class="arrow-icon arrow-icon-next" src="assets/icons/nav-icon-left.svg"/>'
+);
+
+owl.on('translate.owl.carousel', function(e) {
+  var index = e.item.index;
+  $('.card_image').removeClass('animate__animated animate__fadeIn animate__delay-2s');
+  $('.card_image').eq(index).addClass('animate__animated animate__fadeIn animate__delay-2s');
+  $('.card_details').removeClass('animate__animated animate__fadeIn animate__delay-1s');
+  $('.card_details').eq(index).addClass('animate__animated animate__fadeIn animate__delay-1s');
+
+});
+
+});
