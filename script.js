e2e9e7a7604f0ff5dff1887236ab4fdd5cff86ab
Ana Fernández Valdés <aana.fdez@gmail.com>
2022-01-03 09:07:04 +0100

    add autoplay

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
-$(document).ready(function() {
-  var owl = $('.owl-carousel');
+$(document).ready(function () {
+  var owl = $(".owl-carousel");
 
-owl.owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: true,
-  // autoplay:true,
-  // autoplayTimeout:5000,
-  items: 1,
-  animateOut: 'fadeOut'
+  owl.owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    items: 1,
+    animateOut: "fadeOut",
+  });
+  $(".owl-next").html(
+    '<img class="arrow-icon arrow-icon-next" src="assets/icons/nav-icon-right.svg"/>'
+  );
+  $(".owl-prev").html(
+    '<img class="arrow-icon arrow-icon-next" src="assets/icons/nav-icon-left.svg"/>'
+  );
 
-});
-$(".owl-next").html(
-  '<img class="arrow-icon arrow-icon-next" src="assets/icons/nav-icon-right.svg"/>'
-);
-$(".owl-prev").html(
-  '<img class="arrow-icon arrow-icon-next" src="assets/icons/nav-icon-left.svg"/>'
-);
-
-owl.on('translate.owl.carousel', function(e) {
-  var index = e.item.index;
-  $('.card_image').removeClass('animate__animated animate__fadeIn animate__delay-2s');
-  $('.card_image').eq(index).addClass('animate__animated animate__fadeIn animate__delay-2s');
-  $('.card_details').removeClass('animate__animated animate__fadeIn animate__delay-1s');
-  $('.card_details').eq(index).addClass('animate__animated animate__fadeIn animate__delay-1s');
-
-});
-
+  owl.on("translate.owl.carousel", function (e) {
+    var index = e.item.index;
+    $(".card_image").removeClass(
+      "animate__animated animate__fadeIn animate__delay-2s"
+    );
+    $(".card_image")
+      .eq(index)
+      .addClass("animate__animated animate__fadeIn animate__delay-2s");
+    $(".card_details").removeClass(
+      "animate__animated animate__fadeIn animate__delay-1s"
+    );
+    $(".card_details")
+      .eq(index)
+      .addClass("animate__animated animate__fadeIn animate__delay-1s");
+  });
 });
